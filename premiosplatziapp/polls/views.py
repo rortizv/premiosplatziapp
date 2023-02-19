5b25d79e15e9cfd0bbe950e6a166dbe000e43e91
Rafael Ortiz <rortiz@inkremental.co>
2023-02-19 08:26:26 -0500

    index and detail views

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from .models import Question
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.all()
+    return render(request, 'polls/index.html', {
+        'latest_question_list': latest_question_list
+    })
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {
+        'question': question
+    })
+
+
+def results(request, question_id):
+    return HttpResponse(f"You are looking at results from question number {question_id}.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting for question number {question_id}.")
